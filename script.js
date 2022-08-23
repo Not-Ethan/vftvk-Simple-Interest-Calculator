02ddf546f9631cb9f4af5e908c1e23f9a045daa8
Ethan <ethan.wanq@gmail.com>
2022-08-23 23:56:40 +0000

    dynamic rate label update

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
+    let principal = document.getElementById("principal").value;
+    let rate = document.getElementById("rate").value;
+    let years = document.getElementById("years").value;
+    let interest = principal * years * rate/100;
+    let year = new Date().getFullYear()+parseInt(years);
     
+}
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+
 }
         
